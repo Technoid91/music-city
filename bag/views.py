cfcdde52c880c3714b26094edf39091537dfe089
macbook <macbook@macbooks-Air.localdomain>
2024-05-05 17:03:38 +0100

    Added shopping bag app, urls and template

---
+++ bag/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+
+def view_bag(request):
+
+    return render(request, "bag/bag.html")
+
--- music_city/settings.py
+++ music_city/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'home',
     'lessons',
     'products',
+    'bag',
 
     #other
     'crispy_forms',
