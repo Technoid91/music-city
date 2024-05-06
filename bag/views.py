7d141512008aa5a00debad4ec425077762fe26ab
macbook <macbook@macbooks-Air.localdomain>
2024-05-06 22:38:50 +0100

    Added editing and removing items from the bag functionality

--- bag/views.py
+++ bag/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 
 def view_bag(request):
 
     return render(request, "bag/bag.html")
+
 
 def add_to_bag(request, item_id):
     """Add a quantity of the specified product to the shopping bag"""
@@ -20,3 +21,35 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
 
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """Change the quantity of the item in the bag"""
+
+    quantity = int(request.POST.get('quantity'))
+
+    bag = request.session.get('bag', {})
+
+    if quantity > 0:
+        bag[item_id] = quantity
+    else:
+        bag.pop(item_id)
+
+    request.session['bag'] = bag
+
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the bag"""
+    try:
+        bag = request.session.get('bag', {})
+
+        bag.pop(item_id)
+
+        request.session['bag'] = bag
+
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
