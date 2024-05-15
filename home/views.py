2f7c2c84990df7084a2a58ccc243abf94a428191
macbook <macbook@macbooks-Air.localdomain>
2024-05-15 23:06:57 +0100

    Implemented SEO

--- home/views.py
+++ home/views.py
@@ -7,8 +7,8 @@ from lessons.models import Subscription
 def index(request):
     products = Product.objects.filter(old_price__isnull=False)
     products_amount = products.count()
-    if products_amount > 4:
-        products = Product.objects.filter(old_price__isnull=False).order_by('?')[:4]
+    if products_amount > 3:
+        products = Product.objects.filter(old_price__isnull=False).order_by('?')[:3]
     context = {
         'products': products,
     }
