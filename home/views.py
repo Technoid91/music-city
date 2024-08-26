ea3c03b8e2abbe888e354e8b66203431a84605e9
macbook <macbook@macbooks-Air.localdomain>
2024-08-26 19:19:24 +0100

    trying newer version of Django

--- home/views.py
+++ home/views.py
@@ -3,40 +3,8 @@ from django.shortcuts import render
 from products.models import Product
 from lessons.models import Subscription
 
-
-import smtplib
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from django.conf import settings
-
-
-
-
 def index(request):
     """ Render the home page with products with special prices """
-
-    subject = "Your Subject"
-    body = "This is a test email."
-    recipient_email = "technoid91@gmail.com"
-
-    # Создаем сообщение
-    msg = MIMEMultipart()
-    msg['From'] = settings.EMAIL_HOST_USER
-    msg['To'] = recipient_email
-    msg['Subject'] = subject
-    msg.attach(MIMEText(body, 'plain'))
-
-    try:
-        # Устанавливаем соединение с сервером через TLS
-        with smtplib.SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT) as server:
-            server.starttls()  # Устанавливаем защищенное соединение
-            server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
-            # Отправляем письмо
-            server.sendmail(settings.EMAIL_HOST_USER, recipient_email, msg.as_string())
-        print("Test email sent successfully.")
-    except Exception as e:
-        print(f"Failed to send email: {str(e)}")
-
 
     products = Product.objects.filter(old_price__isnull=False)
     products_amount = products.count()
