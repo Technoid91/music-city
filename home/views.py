a0290202e175955077e6b8a3bb7799ec2f56e0dd
macbook <macbook@macbooks-Air.localdomain>
2024-08-26 18:55:12 +0100

    added testing code to the home page to send an email

--- home/views.py
+++ home/views.py
@@ -4,8 +4,39 @@ from products.models import Product
 from lessons.models import Subscription
 
 
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from django.conf import settings
+
+
+
+
 def index(request):
     """ Render the home page with products with special prices """
+
+    subject = "Your Subject"
+    body = "This is a test email."
+    recipient_email = "recipient@example.com"
+
+    # Создаем сообщение
+    msg = MIMEMultipart()
+    msg['From'] = settings.EMAIL_HOST_USER
+    msg['To'] = recipient_email
+    msg['Subject'] = subject
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        # Устанавливаем соединение с сервером
+        server = smtplib.SMTP_SSL(settings.EMAIL_HOST, settings.EMAIL_PORT)
+        server.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+        # Отправляем письмо
+        server.sendmail(settings.EMAIL_HOST_USER, recipient_email, msg.as_string())
+        server.quit()
+    except:
+        pass
+
+
     products = Product.objects.filter(old_price__isnull=False)
     products_amount = products.count()
     if products_amount > 3:
@@ -13,5 +44,4 @@ def index(request):
     context = {
         'products': products,
     }
-
     return render(request, "home/index.html", context)
