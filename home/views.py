11888f57d44563ef6cf19eb9e1595f61178822d9
macbook <macbook@macbooks-Air.localdomain>
2024-08-26 19:35:07 +0100

    trying other versions of django and allauth

--- home/views.py
+++ home/views.py
@@ -4,38 +4,8 @@ from products.models import Product
 from lessons.models import Subscription
 
 
-from django.core.mail import get_connection, EmailMessage
-from django.conf import settings
-
 def index(request):
     """ Render the home page with products with special prices """
-
-    subject = "Your Subject"
-    body = "This is a test email."
-    recipient_email = "technoid91@gmail.com"
-
-    connection = get_connection(
-        backend='django.core.mail.backends.smtp.EmailBackend',
-        host=settings.EMAIL_HOST,
-        port=settings.EMAIL_PORT,
-        username=settings.EMAIL_HOST_USER,
-        password=settings.EMAIL_HOST_PASSWORD,
-        use_tls=settings.EMAIL_USE_TLS,
-    )
-
-    email = EmailMessage(
-        subject,
-        body,
-        settings.DEFAULT_FROM_EMAIL,
-        [recipient_email],
-        connection=connection
-    )
-
-    try:
-        email.send()
-        print("Test email sent successfully.")
-    except Exception as e:
-        print(f"Failed to send email: {str(e)}")
 
     products = Product.objects.filter(old_price__isnull=False)
     products_amount = products.count()
